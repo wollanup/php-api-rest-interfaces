c9a83cbb4297523769af44b4d3c16a86ec8198a5
Steve <steve@eukles.com>
2017-04-04 10:26:01 +0200

    add setQuery method in QueryModifier

--- src/Service/QueryModifier/QueryModifierInterface.php
+++ src/Service/QueryModifier/QueryModifierInterface.php
@@ -12,11 +12,22 @@ use Propel\Runtime\ActiveQuery\ModelCriteria;
 
 interface QueryModifierInterface
 {
-    
+
     /**
+     * Applies modifiers and merge query if one has been set
+     *
      * @param ModelCriteria $query
      *
      * @return ModelCriteria
      */
     public function apply(ModelCriteria $query);
+    
+    /**
+     * This ModelCriteria will be merged with another one when apply is called
+     *
+     * @param ModelCriteria $query
+     *
+     * @return QueryModifierInterface
+     */
+    public function setQuery(ModelCriteria $query);
 }
